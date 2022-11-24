8f1d2dce12374fdff88c4baa0bac42c51dfb8e83
sdcb <sdflysha@qq.com>
2022-11-24 16:55:59 +0800

    添加项目文件。

---
+++ paddlesharp-ocr-aspnetcore-demo/Controllers/OcrController.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using OpenCvSharp;
+using Sdcb.PaddleInference;
+using Sdcb.PaddleOCR.Models.LocalV3;
+using Sdcb.PaddleOCR.Models;
+using Sdcb.PaddleOCR;
+
+namespace paddlesharp_ocr_aspnetcore_demo.Controllers
+{
+    public class OcrController
+    {
+        [Route("ocr")]
+        public string Ocr(IFormFile file)
+        {
+            PaddleConfig.Defaults.UseMkldnn = true;
+            FullOcrModel model = LocalFullModels.ChineseV3;
+
+            using MemoryStream ms = new();
+            using Stream stream = file.OpenReadStream();
+            stream.CopyTo(ms);
+            using Mat src = Cv2.ImDecode(ms.ToArray(), ImreadModes.Color);
+            double scale = 1;
+            using Mat scaled = src.Resize(Size.Zero, scale, scale);
+
+            using PaddleOcrAll all = new(model)
+            {
+                Enable180Classification = true,
+                AllowRotateDetection = true,
+            };
+
+            return all.Run(scaled).Text;
+        }
+    }
+}
---
+++ paddlesharp-ocr-aspnetcore-demo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace paddlesharp_ocr_aspnetcore_demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
