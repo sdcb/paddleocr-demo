1879a159d1cda45688b0e943c438a30a2509c4c2
sdcb <sdflysha@qq.com>
2023-04-03 16:09:51 +0800

    Add document.

--- paddlesharp-ocr-aspnetcore-demo/Controllers/HomeController.cs
+++ paddlesharp-ocr-aspnetcore-demo/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace paddlesharp_ocr_aspnetcore_demo.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
